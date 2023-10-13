6ea755a522d2422f44233484228df5a107ee8fb6
joseenriquemg <josee.munoz@iesnervion.es>
2023-10-13 14:43:37 +0200

    ejercicios

--- src/tema2boletin3/Ejercicio06.java
+++ src/tema2boletin3/Ejercicio06.java
@@ -6,13 +6,16 @@ public class Ejercicio06 {
 
 	public static void main(String[] args) {
 		int edad =0;
-		
+		int edadAlumno = 0;
+		int mayorEdad = 0;
 		Scanner sc = new Scanner(System.in);
 		
 		while (edad >=0) {
 			System.out.println("Introduzca su edad. Numero negativo para cerrar.");
 			edad = sc.nextInt();
-
-		}}}
+			edadAlumno++;
+			if (edad >= 18) {
+			mayorEdad++;
+		}}}}
 
 
